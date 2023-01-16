8ecc42f9c7bcc589b7401addee68b5b9cfae3ee8
Rishabh-Singh-Codes <rishabhsinghcodes@gmail.com>
2023-01-16 20:49:06 +0530

    Create sort-the-people.js

---
+++ sort-the-people.js
@@ -0,0 +1,23 @@
+// You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+// For each index i, names[i] and heights[i] denote the name and height of the ith person.
+// Return names sorted in descending order by the people's heights.
+
+// Link: https://leetcode.com/problems/sort-the-people/description/
+
+//1st soln O(n)
+
+var sortPeople = function(names, heights) {
+    sortedHeightMap = {};
+
+    for(let i =0; i<heights.length; i++){
+        sortedHeightMap[heights[i]] = i;
+    }
+
+    let finalArr = [];
+
+    Object.keys(sortedHeightMap).map((el) => {
+        finalArr.push(names[sortedHeightMap[el]])
+    })
+
+    return finalArr.reverse();
+};
