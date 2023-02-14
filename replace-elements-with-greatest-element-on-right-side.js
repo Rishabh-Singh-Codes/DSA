5dddf32d915e42a859294d31b21e36a8fac6747c
Rishabh-Singh-Codes <rishabhsinghcodes@gmail.com>
2023-02-14 14:54:04 +0530

    Update replace-elements-with-greatest-element-on-right-side.js

--- replace-elements-with-greatest-element-on-right-side.js
+++ replace-elements-with-greatest-element-on-right-side.js
@@ -15,3 +15,19 @@ var replaceElements = function(arr) {
 
     return res;
 };
+
+//2nd soln O(n) w/ help
+
+var replaceElements = function(arr) {
+    let max = arr[arr.length - 1];
+    arr[arr.length - 1] = -1;
+
+    for(let i = arr.length-2; i>=0;i--){
+        let curr = arr[i];
+        arr[i] = max;
+        if (curr > max) max = curr;
+    }
+
+    return arr;
+    
+};
