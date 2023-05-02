26d70022af0094b864c47cc59445e100ffb98e94
Rishabh-Singh-Codes <rishabhsinghcodes@gmail.com>
2023-05-02 15:45:34 +0530

    Create maximum-nesting-depth-of-the-parentheses.js

---
+++ maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,32 @@
+// A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+// It is an empty string "", or a single character not equal to "(" or ")",
+// It can be written as AB (A concatenated with B), where A and B are VPS's, or
+// It can be written as (A), where A is a VPS.
+// We can similarly define the nesting depth depth(S) of any VPS S as follows:
+// depth("") = 0
+// depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+// depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+// depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+// For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
+// Given a VPS represented as string s, return the nesting depth of s.
+
+// Link: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/
+
+//1st soln O(n)
+
+var maxDepth = function(s) {
+    let max = 0;
+    let curr = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === '(') {
+            curr++;
+            max = Math.max(curr, max);
+        } else if(s[i] === ')') {
+            curr--;
+        }
+    }
+
+    return max;
+
+};
