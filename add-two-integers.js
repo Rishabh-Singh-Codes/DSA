cff669e1d97cd7344eab94121f4d3c1bcda463ce
Rishabh-Singh-Codes <rishabhsinghcodes@gmail.com>
2023-06-25 19:43:55 +0530

    Create add-two-integers.js

---
+++ add-two-integers.js
@@ -0,0 +1,10 @@
+// Given two integers num1 and num2, return the sum of the two integers.
+// [Why is it even on LC?] 
+
+// Link: https://leetcode.com/problems/add-two-integers/description/
+
+//1st soln O(n)
+
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
