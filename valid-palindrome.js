8415dd4bee7ccf37b1bc5cedfd3a310add286c13
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-10-16 21:32:56 +0530

    Create valid-palindrome.js

---
+++ valid-palindrome.js
@@ -0,0 +1,18 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Link: https://leetcode.com/problems/valid-palindrome/description/
+
+//1st soln O(n)
+
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi, "");
+
+    for(let i = 0, j = s.length - 1; i <= j; i++, j--) {
+        if(s.charAt(i) !== s.charAt(j)) {
+            return false;
+        }
+    }
+
+    return true;
+};
