e29004464d1a590dc68cf79b95361ca13bedc581
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-10-26 20:01:05 +0530

    Create power-of-two.js

---
+++ power-of-two.js
@@ -0,0 +1,25 @@
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+// Link: https://leetcode.com/problems/power-of-two/description/
+
+//1st soln O(n)
+
+var isPowerOfTwo = function(n) {
+    if(n === 0) {
+        return false;
+    }
+
+    if(n === 1) {
+        return true;
+    }
+
+    while(n > 1) {
+        n /= 2;
+        if(n === 1) {
+            return true;
+        }
+    }
+
+    return false;
+};
