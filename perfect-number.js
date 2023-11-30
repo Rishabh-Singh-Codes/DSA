704638fa902b66c78dfe090af2bf431e301be2ba
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-11-30 20:34:46 +0530

    Create perfect-number.js

---
+++ perfect-number.js
@@ -0,0 +1,22 @@
+// A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+// Given an integer n, return true if n is a perfect number, otherwise return false.
+
+// Link: https://leetcode.com/problems/perfect-number/description/
+
+//1st soln O(n) w/ help
+
+var checkPerfectNumber = function(num) {
+    if(num <= 1) {
+        return false;
+    }
+
+    let sum = 0;
+
+    for(let i = 1; i <= Math.floor(Math.sqrt(num)); i++) {
+        if(num%i === 0) {
+            sum += i + (num/i);
+        }
+    }
+
+    return sum === 2 * num;
+};
