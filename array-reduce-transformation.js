ec3500fefd23117df9547d5e2800f5598b73d0e2
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-12-07 14:57:56 +0530

    Create array-reduce-transformation.js

---
+++ array-reduce-transformation.js
@@ -0,0 +1,17 @@
+// Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+// A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+// If the length of the array is 0, it should return init.
+// Please solve it without using the built-in Array.reduce method.
+
+// Link: https://leetcode.com/problems/array-reduce-transformation/description/
+
+//1st soln O(n) w/ help
+
+var reduce = function(nums, fn, init) {
+    let res = init;
+    for(let i = 0; i < nums.length; i++) {
+        res = fn(res, nums[i]);
+    }
+
+    return res;
+};
