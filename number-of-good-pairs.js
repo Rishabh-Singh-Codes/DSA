32e53832230de04a819be9212b5b51cf5df0f00b
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-01-27 17:25:21 +0530

    Update number-of-good-pairs.js

--- number-of-good-pairs.js
+++ number-of-good-pairs.js
@@ -54,3 +54,21 @@ let numIdenticalPairs = (nums) => {
 
     return count;
 }
+
+//Prac 2
+
+var numIdenticalPairs = function(nums) {
+    let map = {};
+    let res = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(map[nums[i]]) {
+            res += map[nums[i]];
+            map[nums[i]] += 1;
+        } else {
+            map[nums[i]] = 1;
+        }
+    }
+
+    return res;
+};
