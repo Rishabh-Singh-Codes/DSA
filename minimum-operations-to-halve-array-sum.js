ffcb6bc006da91c48caa42cbd91369f3a3670336
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-02-29 22:02:34 +0530

    Updated minimum-operations-to-halve-array-sum

--- minimum-operations-to-halve-array-sum.js
+++ minimum-operations-to-halve-array-sum.js
@@ -22,3 +22,82 @@ var halveArray = function(nums) {
 
     return res;
 };
+
+
+//2nd try O(n^2) [TLE] w/ help
+
+var halveArray = function(nums) {
+    let res = 0;
+
+    nums.sort((a, b) => a - b);
+
+    let sum = nums.reduce((acc, curr) => acc + curr, 0);
+
+    let currSum = sum;
+
+    while(currSum * 2 > sum) {
+        res++;
+        const largestNum = nums.pop();
+        const halfVal = largestNum/2;
+
+        currSum -= halfVal;
+
+        if(nums.length === 0) {
+            nums.push(halfVal);
+        } else if(halfVal >= nums[nums.length - 1]) {
+            nums.push(halfVal);
+        } else if(halfVal <= nums[0]) {
+            nums.unshift(halfVal);
+        } else {
+            let left = 0;
+            let right = nums.length;
+
+            while(left <= right) {
+                const arrLen = right - left;
+                const midIdx = left + Math.floor(arrLen / 2);
+
+                const val = nums[midIdx];
+
+                if(val === halfVal) {
+                    left = midIdx;
+                    right = midIdx;
+                    break;
+                }
+
+                if(halfVal > val) {
+                    left = midIdx + 1;
+                } else {
+                    right = midIdx - 1;
+                }
+            }
+
+            nums.splice(left, 0, halfVal);
+        }
+    }
+
+    return res;
+};
+
+
+// 1st Soln O(nlogn) w/ help
+
+var halveArray = function(nums) {
+    let mPQ = new MaxPriorityQueue();
+
+    nums.forEach(num => mPQ.enqueue(num));
+
+    let sum = nums.reduce((acc, curr) => acc + curr, 0);
+
+    let halfSum = sum/2;
+    let res = 0;
+
+    while(sum > halfSum) {
+        res++;
+        let maxEle = mPQ.dequeue();
+        let halfEle = maxEle.element / 2;
+        sum -= halfEle;
+        mPQ.enqueue(halfEle);
+    }
+
+    return res;
+};
