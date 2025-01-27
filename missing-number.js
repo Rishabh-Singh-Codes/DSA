8995b9e42c2826fe4597fb3707ab4fd7f626b4b7
Rishabh Singh <rishabhsinghcodes@gmail.com>
2025-01-27 22:06:55 +0530

    updated missing-number

--- missing-number.js
+++ missing-number.js
@@ -2,7 +2,7 @@
 
 // Link: https://leetcode.com/problems/missing-number/description/
 
-//1st soln O(n)
+//1st soln O(nlogn)
 
 var missingNumber = function(nums) {
     nums.sort((a, b) => a-b);
@@ -12,4 +12,21 @@ var missingNumber = function(nums) {
     }
 
     return nums.length;
+};
+
+
+//2nd soln O(n)
+
+var missingNumber = function(nums) {
+    let map = {};
+
+    for(let i = 0; i < nums.length; i++) {
+        map[nums[i]] = 1;
+    }
+
+    for (let i = 0; i <= nums.length; i++) {
+        if(map[i] === undefined) {
+            return i;
+        }
+    }
 };
